d170f23bad3ffdf81728549c43add9aad0cfeeaf
Parham <parham2002sh@gmail.com>
2022-04-11 03:37:24 -0700

    function 11 is done

--- String_startsWith_2.s
+++ String_startsWith_2.s
@@ -1,32 +1,30 @@
-    .global String_startsWith_1
+    .global String_startsWith_2
 
     .text
 
 // PRE-CONDITION
 // X0 holds the address to the string
-// X1 holds the index
-// X2 holds the address to the phrase
+// X1 holds the address to the phrase
 
 // POST-CONDITION
 // X0 holds 1 for the phrase found in the string
 // and 0 for the phrase not found in the string
 
-String_startsWith_1:
-    MOV X19, X0
-    MOV X20, X1
-    MOV X21, X2
-
-    ADD     X19, X19, X20
+String_startsWith_2:
+//    MOV X19, X0
+//    MOV X20, X2
+    LDR     X19, =str1
+    LDR     X20, =str2
 
     LDRB    W3, [X19], #1
-    LDRB    W4, [X21], #1
+    LDRB    W4, [X20], #1
 
 Loop:
     CBZ     W4, found
     CMP     W3, W4
     BNE     notFound
     LDRB    W3, [X19], #1
-    LDRB    W4, [X21], #1
+    LDRB    W4, [X20], #1
     B       Loop
 
 notFound:
@@ -43,7 +41,7 @@ exit:
     MOV X8, #93                     //Service command code 93 terminates this program
     SVC 0                           //Call linux to output the string
 
-    .end                                //End program
+    .end                            //End program
 
 
 
