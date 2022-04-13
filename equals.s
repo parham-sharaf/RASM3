bec106405620d034e835e2517504aa24f4e0de44
ssushiseeth <4ktrans4@gmail.com>
2022-04-13 07:42:27 +0100

    Functional String_equals function

--- equals.s
+++ equals.s
@@ -1,30 +1,36 @@
-    .global equals                      //Provide program starting address to linker
+    .global equals              //Provide program starting address to linker
 
     .text
 
-    //X3 holds the first string
-    //X4 holds the second string
+//FUNCTION equals======================================================================
+//@PARAM:
+//X0 holds the first string to be compared
+//X1 holds the second string to be compared
+//
+//@RETURN:
+//X0 holds the boolean (1 = TRUE, 0 = FALSE)
+//=====================================================================================
 
 equals:
-        ldrb W5, [X3], #1
-        ldrb W6, [X4], #1
+        ldrb W2, [X0], #1       //Loading W2 with the next byte from the first string
+        ldrb W3, [X1], #1       //Loading W3 with the next byte from the second string
 
-        cmp W5, #0              //Checking if the character is a null character
-        b.eq checkOther              //If it is a null character, return back
+        cmp W2, #0              //Checking if the character from the first string is a null character
+        b.eq checkOther         //If it is a null character, check to see if we are at the end of the second string
 
-        cmp W5, W6
-        b.ne exit
+        cmp W2, W3              //Checking to see if the character from the first string and second string match
+        b.ne exit               //If they do not match, jump to exit (NOT setting the boolean to TRUE)
 
-        b equals
+        b equals                //Unconditional jump to equals
 
 checkOther:
-        cmp W6, #0              //Checking if the character is a null character
-        b.eq same               //If it is a null character, return back
+        cmp W3, #0              //Checking if the character is a null character
+        b.eq same               //If it is a null character, jump to same
 
-        b exit
+        b exit                  //Unconditional jump to exit
 
 same:
-        mov X6, #1
+        mov X0, #1              //Setting the boolean to TRUE
 
 exit:
-        ret
+        ret                     //Return back to main
