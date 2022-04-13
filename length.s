d961e41b98c06a7c602f970d858cd1f103069b68
ssushiseeth <4ktrans4@gmail.com>
2022-04-13 07:16:33 +0100

    test

--- length.s
+++ length.s
@@ -1,19 +1,26 @@
-    .global length                      //Provide program starting address to linker
+    .global length              //Provide program starting address to linker
 
     .text
 
-//X4 holds the string
-//X0 holds the counter
+//FUNCTION length======================================================================
+//@PARAM:
+//X0 holds the string whose length is to be determined
+//X1 holds the counter
+//
+//@RETURN:
+//X0 holds the length of the string
+//=====================================================================================
 
 length:
-        ldrb W5, [X4], #1
+        ldrb W2, [X0], #1       //Loading W2 with the next byte from the string
 
-        cmp W5, #0              //Checking if the character is a null character
-        b.ne cont               //If it is a null character, return back
+        cmp W2, #0              //Checking if the character is a null character
+        b.ne cont               //If it is a null character, jump to cont
 
-        ret
+        mov X0, X1              //Moving the counter from X1 to X0 (facilitates printing it out to the terminal)
+
+        ret                     //Return back to main
 cont:
-        //Increment counter
-        add X0, X0, #1
+        add X1, X1, #1          //Increment counter by 1
 
-        b length
+        b length                //Unconditional jump to length
