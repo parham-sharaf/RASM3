9e13c894178ea28e574fb5714e9a7a4a31720fe7
Parham <parham2002sh@gmail.com>
2022-04-21 15:05:25 -0700

    changed String_lastIndexOf_1 to preserve X19-X29 registers

--- String_lastIndexOf_1.s
+++ String_lastIndexOf_1.s
@@ -9,30 +9,27 @@
 // X0 returns the index of last occurance of the character
 
 String_lastIndexOf_1:
-    MOV     X19, X0
-    MOV     X20, X1
-
     STR     LR, [SP, #-16]!
-    MOV     X0, X19
+
     BL      length
     LDR     LR, [SP], #16
 
-    SUB     W3, W0, #1
-    ADD     X19, X19, X3
+    SUB     W2, W0, #1
+    ADD     X0, X0, X3
 
-    LDRB    W4, [X19], #-1
+    LDRB    W3, [X0], #-1
 
 loop:
-    CMP     W3, #-1
+    CMP     W2, #-1
     BEQ     notFound
-    CMP     W20, W4
+    CMP     W1, W3
     BEQ     found
     SUB     W3, W3, #0x1
-    LDRB    W4, [X19], #-1
+    LDRB    W4, [X0], #-1
     B       loop
 
 found:
-    MOV     X0, X3
+    MOV     X0, X2
     RET
 
 notFound:
--- String_lastIndexOf_2.s
+++ String_lastIndexOf_2.s
@@ -10,22 +10,19 @@
 // X0 returns the index of last occurance of the character
 
 String_lastIndexOf_2:
-    LDR     X19, =name
-    MOV     X20, 0x47
-    MOV     X21, #6
 
-    MOV     X3, X21
-    ADD     X19, X19, X21
+    MOV     X3, X1
+    ADD     X0, X0, X1
 
-    LDRB    W4, [X19], #-1
+    LDRB    W4, [X0], #-1
 
 loop:
     CMP     W3, #-1
     BEQ     notFound
-    CMP     W20, W4
+    CMP     W1, W4
     BEQ     found
     SUB     W3, W3, #0x1
-    LDRB    W4, [X19], #-1
+    LDRB    W4, [X0], #-1
     B       loop
 
 found:
